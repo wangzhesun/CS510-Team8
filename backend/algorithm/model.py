183c3087fcf42e29a118cd586e596025c2477d6a
LujiajieShen <260371471sl@gmail.com>
2024-05-05 20:46:04 -0500

    model

--- backend/algorithm/model.py
+++ backend/algorithm/model.py
@@ -1,3 +1,12 @@
+import pandas as pd
+
+def read_file(train_file,test_file):
+    training = pd.read_csv(train_file)
+    testing = pd.read_csv(test_file)
+
+read_file('Training.csv',"Testing.csv")
+
+
 class ModelName:
     def __init__(self):
         self.variable = 1
