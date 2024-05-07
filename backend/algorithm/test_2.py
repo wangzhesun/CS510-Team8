477fa91b553d514bcfa6f7a184e0ee29be91d9a8
SS-CC-YY <songchenyu2@gmail.com>
2024-05-06 22:59:37 -0500

    update test_2.py

--- backend/algorithm/test_2.py
+++ backend/algorithm/test_2.py
@@ -11,7 +11,7 @@ from nltk.tokenize import word_tokenize
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
-threshold = 0.99
+threshold = 0.8
 final_keywords = set()
 potential_keywords = set()
 
@@ -70,7 +70,8 @@ seg_user_input=chunk_sentence(user_input)
 # user_input = "I have been dealing with severe headaches and dizziness for the past three days. It's worse in the morning. I also notice that my vision blurs occasionally, and I feel nauseous from time to time."
 symptoms = training.columns
 symptoms = symptoms[0:-1]
-keywords = symptoms.tolist()
+# keywords = symptoms.tolist()
+keywords = [symptom.replace('_', ' ') for symptom in symptoms]
 
 input_embeds = [get_embedding(text) for text in seg_user_input]
 keyword_embeds = [get_embedding(keyword) for keyword in keywords]
@@ -83,23 +84,23 @@ for j in range(len(input_embeds)):
 	bert_scores = {keywords[i]: cosine_similarity(input_embeds[j], keyword_embeds[i]).item() for i in range(len(keywords))}
 	bert_scores_list.append(bert_scores)
 	
-	# texts = [seg_user_input[j]] + keywords
-	# vectorizer = TfidfVectorizer()
-	# tfidf_matrix = vectorizer.fit_transform(texts)
-	# tfidf_array = tfidf_matrix.toarray()
-	# keyword_tfidf_scores = {keywords[i]: tfidf_array[0] @ tfidf_array[i + 1] for i in range(len(keywords))}
-	# keyword_tfidf_scores_list.append(keyword_tfidf_scores)
+	texts = [seg_user_input[j]] + keywords
+	vectorizer = TfidfVectorizer()
+	tfidf_matrix = vectorizer.fit_transform(texts)
+	tfidf_array = tfidf_matrix.toarray()
+	keyword_tfidf_scores = {keywords[i]: tfidf_array[0] @ tfidf_array[i + 1] for i in range(len(keywords))}
+	keyword_tfidf_scores_list.append(keyword_tfidf_scores)
 	
 	combined_scores = {}
 	for keyword in keywords:
-		# combined_score = 1.0 * bert_scores[keyword] + 0.0 * keyword_tfidf_scores[keyword]
-		combined_score = 1.0 * bert_scores[keyword] 
+		combined_score = 0.7 * bert_scores[keyword] + 0.3 * keyword_tfidf_scores[keyword]
+		# combined_score = 1.0 * bert_scores[keyword] 
 		combined_scores[keyword] = combined_score
 	combined_scores_list.append(combined_scores)
 
 for j in range(len(input_embeds)):
 	print(f"Input {j+1}:")
-	sorted_keywords = sorted(combined_scores_list[j].items(), key=lambda x: x[1], reverse=True)[:1]
+	sorted_keywords = sorted(combined_scores_list[j].items(), key=lambda x: x[1], reverse=True)[:2]
 	for keyword, score in sorted_keywords:
 		print(f"{keyword}: {score}")
 		potential_keywords.add(keyword)
@@ -109,4 +110,5 @@ for j in range(len(input_embeds)):
 
 if not final_keywords:
 	final_keywords = potential_keywords
+final_keywords = {keyword.replace(' ', '_') for keyword in final_keywords}
 print(final_keywords)
